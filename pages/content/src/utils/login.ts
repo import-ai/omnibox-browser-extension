03da9737eb998a0e8ff2201cd29d1d21c13393c9
hewenguang <wenguang.fe@gmail.com>
2025-08-20 16:04:12 +0800

    fix(comment): fix comments

--- pages/content/src/utils/login.ts
+++ pages/content/src/utils/login.ts
@@ -1,34 +1,36 @@
-import { ready } from '@src/utils/ready';
-
 // 登录页面
 if (location.search === '?from=extension') {
-  ready(() => {
-    chrome.storage.sync.get('apiKey', data => {
-      if (data.apiKey) {
-        return;
-      }
-      document.body.setAttribute('data-from-extension', 'true');
-      const observer = new MutationObserver(mutations => {
-        for (const mutation of mutations) {
-          if (mutation.type === 'attributes' && mutation.attributeName === 'data-token') {
-            const token = document.body.getAttribute('data-token');
-            if (token) {
-              observer.disconnect();
-              document.body.removeAttribute('data-from-extension');
-              document.body.removeAttribute('data-token');
-              chrome.runtime.sendMessage({
-                action: 'saveToken',
-                token,
-              });
-            }
+  chrome.storage.sync.get('apiKey', data => {
+    if (data.apiKey) {
+      return;
+    }
+    const token = document.body.getAttribute('data-token');
+    if (token) {
+      document.body.removeAttribute('data-token');
+      chrome.runtime.sendMessage({
+        action: 'saveToken',
+        token,
+      });
+    }
+    const observer = new MutationObserver(mutations => {
+      for (const mutation of mutations) {
+        if (mutation.type === 'attributes' && mutation.attributeName === 'data-token') {
+          const token = document.body.getAttribute('data-token');
+          if (token) {
+            observer.disconnect();
+            document.body.removeAttribute('data-token');
+            chrome.runtime.sendMessage({
+              action: 'saveToken',
+              token,
+            });
           }
         }
-      });
-      observer.observe(document.body, {
-        subtree: false,
-        attributes: true,
-        attributeFilter: ['data-token'],
-      });
+      }
+    });
+    observer.observe(document.body, {
+      subtree: false,
+      attributes: true,
+      attributeFilter: ['data-token'],
     });
   });
 }
